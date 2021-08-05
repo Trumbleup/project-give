5ac828ae012978c068fc78cf91d2c4779872df21
Danny <Dtrumble12@gmail.com>
2021-08-04 19:30:58 -0500

    First Commit, using Nextjs. Setup the Layout Component. Initialized the first API call to Charity Navigator

---
+++ components/Navbar/Navbar.component.jsx
@@ -0,0 +1,9 @@
+import styles from "./Navbar.module.scss";
+
+export default function Navbar() {
+    return (
+        <div className={styles.navbar}>
+            <h1>Project-Give</h1>
+        </div>
+    )
+}
